29f6d861ddc712daaebed728f4830e8610c056ff
Matthew Spratlin <mkspratlin01@bvsd.org>
2024-08-30 17:30:24 -0600

    Fixed Issues

--- main.py
+++ main.py
@@ -3,11 +3,11 @@ import sys
 import vex
 import constants.constants as constants
 
-#region config
+# region config
 joystick = vex.Joystick()
 leftMotor = vex.Motor(constants.LEFT_MOTOR_ID, constants.LEFT_MOTOR_POLARITY)
 rightMotor = vex.Motor(constants.RIGHT_MOTOR_ID, constants.RIGHT_MOTOR_POLARITY)
-#endregion config
+# endregion config
 
 leftMotor.run(joystick.axis3())
 rightMotor.run(joystick.axis2())
