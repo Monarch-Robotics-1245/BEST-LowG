9ae53ba34581999a39d39b9d81b0cce3e914cfb0
Matthew Spratlin <mkspratlin01@bvsd.org>
2024-09-16 19:46:45 +0000

    Delete sevo.py

--- subsystems/sevo.py
+++
@@ -1,8 +0,0 @@
-def servo(sevenU, sevenL, priviousServoOne)
-    if sevenU == 1
-        servoOne = 127
-    elif sevenL ==1
-        servoOne = 0
-    else
-        servoOne = priviousServoOne
-        
