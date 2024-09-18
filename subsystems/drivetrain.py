561a6ac62f8700d69eaec766b02e34fd5bc6437b
Matthew Spratlin <mkspratlin01@bvsd.org>
2024-09-17 21:55:23 -0600

    replaced headder comments in code

--- subsystems/drivetrain.py
+++ subsystems/drivetrain.py
@@ -1,31 +1,31 @@
 #!/usr/bin/env python3
 
-""" Standard Declaration
-leftMotor = 0
-rightMotor = 0
-motorOne = 0
-motorTwo = 0
-servoOne = 0
-servoTwo = 0
-servoThree = 0
-servoFour = 0
-axisOne = 0
-axisTwo = 0
-axisThree = 0
-axisFour = 0
-fiveU = 0
-fiveD = 0
-sixU = 0
-sixD = 0
-sevenU = 0
-sevenL = 0
-sevenR = 0
-sevenD = 0
-eightR = 0
-eightD = 0
-eightU = 0
-eightL = 0
-End Standard Declaration """
+# Standard Declaration
+# leftMotor = 0
+# rightMotor = 0
+# motorOne = 0
+# motorTwo = 0
+# servoOne = 0
+# servoTwo = 0
+# servoThree = 0
+# servoFour = 0
+# axisOne = 0
+# axisTwo = 0
+# axisThree = 0
+# axisFour = 0
+# fiveU = 0
+# fiveD = 0
+# sixU = 0
+# sixD = 0
+# sevenU = 0
+# sevenL = 0
+# sevenR = 0
+# sevenD = 0
+# eightR = 0
+# eightD = 0
+# eightU = 0
+# eightL = 0
+# End Standard Declaration
 
 
 def drive(axisTwo, axisThree):
