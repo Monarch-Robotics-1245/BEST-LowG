85b71b03845753b6dcb66e0af0efa7e55e2b3e45
Matthew Spratlin <mkspratlin01@bvsd.org>
2024-09-23 11:20:47 -0600

    Updated gitignore to not store compiled programs Created Command, Subsystem, and Auto scripts for the Arm, Turret, and Gripper Inserted standard header into all of those files

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-import commands.drivetrain
+import gripper.drivetrain
 
 
 def robot(axisOne, axisTwo, axisThree, axisFour, fiveU, fiveD, sixU, sixD, sevenU, sevenL, sevenR, sevenD, eightR, eightD, eightU, eightL, previousEightR, previousEightL, leftMotor, rightMotor, motorOne, motorTwo, servoOne, servoTwo, servoThree, servoFour, direction, speedMultiplier):
@@ -10,6 +10,6 @@ def robot(axisOne, axisTwo, axisThree, axisFour, fiveU, fiveD, sixU, sixD, seven
     FAST_SPEED = 1
 
     # 0 (not pressed) and 1(pressed) - not True and False for buttons
-    commands.drivetrain.drive(direction, speedMultiplier, axisOne, axisTwo, eightR, eightL, previousEightR, previousEightL, SLOW_SPEED, FAST_SPEED)
+    gripper.drivetrain.drive(direction, speedMultiplier, axisOne, axisTwo, eightR, eightL, previousEightR, previousEightL, SLOW_SPEED, FAST_SPEED)
 
     return leftMotor, rightMotor, motorOne, motorTwo, servoOne, servoTwo, servoThree, servoFour, direction, speedMultiplier
