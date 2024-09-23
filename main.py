a3932043cab7ece01de577a8d350637cb192052c
Matthew Spratlin <mkspratlin01@bvsd.org>
2024-09-23 17:44:37 +0000

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-import gripper.drivetrain
+import commands.drivetrain
 
 
 def robot(axisOne, axisTwo, axisThree, axisFour, fiveU, fiveD, sixU, sixD, sevenU, sevenL, sevenR, sevenD, eightR, eightD, eightU, eightL, previousEightR, previousEightL, leftMotor, rightMotor, motorOne, motorTwo, servoOne, servoTwo, servoThree, servoFour, direction, speedMultiplier):
@@ -10,6 +10,6 @@ def robot(axisOne, axisTwo, axisThree, axisFour, fiveU, fiveD, sixU, sixD, seven
     FAST_SPEED = 1
 
     # 0 (not pressed) and 1(pressed) - not True and False for buttons
-    gripper.drivetrain.drive(direction, speedMultiplier, axisOne, axisTwo, eightR, eightL, previousEightR, previousEightL, SLOW_SPEED, FAST_SPEED)
+    commands.drivetrain.drive(direction, speedMultiplier, axisOne, axisTwo, eightR, eightL, previousEightR, previousEightL, SLOW_SPEED, FAST_SPEED)
 
     return leftMotor, rightMotor, motorOne, motorTwo, servoOne, servoTwo, servoThree, servoFour, direction, speedMultiplier
